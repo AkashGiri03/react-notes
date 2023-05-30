fc934262de5ab97d71c1da5a33f742874637f44e
AkashGiri03 <113370221+AkashGiri03@users.noreply.github.com>
2023-05-30 18:38:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Notes from "./Components/Notes/Notes";
+import SideNav from "./Components/SideNav/SideNav";
+import './App.css'
+export default function App() {
 
-function App() {
+  
+  const items = JSON.parse(localStorage.getItem("notes") || '[]');
+
+  const [addNote, setAddNote] = useState(items);
+  const [favorite, setFavorite] = useState(false)
+  const [delAlert, setDeletAlert] = useState(false)
+
+  const handleAlert = () =>{
+    setDeletAlert(true)
+  }
+  
+  const handleAdd = (color) => {
+    setAddNote([
+      ...addNote,
+      {
+        note: "",
+        color: color,
+        currDate: new Date().toLocaleString('en-GB'),
+        favorite: false,
+        pinned: false
+      }
+    ]);
+  };
+  const handleAddText = (e, index) => {
+    
+    addNote[index].note = e.target.value;
+    localStorage.setItem("notes", JSON.stringify(addNote));
+    
+  };
+  const handleFavorite = (index) =>{
+    if(!addNote[index].favorite){
+      addNote[index].favorite = true
+    }
+    else{
+      addNote[index].favorite = false
+    }
+    setFavorite(!favorite)
+}
+
+  const handlePin = (notes, index) => {
+    if(!addNote.pinned){
+      addNote[index].pinned = true
+      let pinned = addNote.filter((item) => item.notes !== notes);
+      pinned.unshift(addNote[index]);
+      pinned.splice(index + 1, 1);
+      setAddNote(pinned);
+    }
+    else{
+      addNote[index].pinned = false
+    }
+  };
+
+  const handleDelete = (index) => {
+    console.log('ls', items[index])
+    // const deleteItem = items[index]
+    addNote.splice(index, 1);
+    items.splice(index, 1);
+    handleAlert()
+  };
+
+  useEffect(() => {
+    setTimeout(()=>{
+      setDeletAlert(false)
+    },2000)
+    localStorage.setItem("notes", JSON.stringify(addNote));
+  }, [addNote, favorite, delAlert]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        delAlert ? <div className="deleteAlert">Note Deleted</div> : null
+      }
+      
+      <SideNav handleAdd={handleAdd}/>
+      <Notes noteItems={addNote} handleAddText={handleAddText} handleFavorite={handleFavorite} handleDelete={handleDelete} handlePin={handlePin}/>
     </div>
   );
 }
-
-export default App;
